0eb5f97b3a63c88a08de9b7de73a6565784bf45c
Casey Wylie <cwylie@redhat.com>
2020-06-14 18:57:46 -0400

    format

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ["<rootDir>/src"],
   coveragePathIgnorePatterns: [
     "<rootDir>/src/__mocks__/*",
-    "<rootDir>/src/routes/*"
+    "<rootDir>/src/routes/*",
   ],
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
