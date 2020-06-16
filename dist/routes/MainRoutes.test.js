a0f4c00957ac6f9a2cd1310364736907e361b31a
Casey Wylie <cwylie@redhat.com>
2020-06-16 12:23:02 -0400

    delete unused code

--- dist/routes/MainRoutes.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mainRoutes_1 = require("./mainRoutes");
-describe("MainRoutes class", () => {
-    it("should return an instance of the routes", () => {
-        expect(mainRoutes_1.mainRoutes).toBeDefined();
-    });
-});
-//# sourceMappingURL=MainRoutes.test.js.map
